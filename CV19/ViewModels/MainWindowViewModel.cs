c84f499829a50b2437e3b3eb85396f24ba323e7c
rogachev-pavel <106842400+rogachev-pavel@users.noreply.github.com>
2022-07-08 20:07:41 +0300

    Добавьте файлы проекта.

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+
+        #region Заголовок окна
+
+        /// <summary> Заголовок окна </summary>
+        private string _Title ="Анализ статистики CV19";
+        public string Title
+        { 
+            get => _Title;
+            set => Set(ref _Title, value); 
+        }
+        #endregion
+    }
+}
---
+++ Test/CV19Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CV19Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
